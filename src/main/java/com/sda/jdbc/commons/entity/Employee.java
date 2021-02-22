1eef3a1b65911315d733ebab672470bf33b3b778
Karol011 <karollacek1@gmail.com>
2021-02-22 11:32:47 +0100

    implemented findByID method

--- src/main/java/com/sda/jdbc/commons/entity/Employee.java
+++ src/main/java/com/sda/jdbc/commons/entity/Employee.java
@@ -1,4 +1,24 @@
 package com.sda.jdbc.commons.entity;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Employee {
+
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate hireDate;
+    private String jobId;
+    private Double salary;
+    private Double commissionPct;
+    private Integer managerId;
+    private Integer departmentId;
 }
--- src/main/java/com/sda/jdbc/todo/EmployeesDAO.java
+++ src/main/java/com/sda/jdbc/todo/EmployeesDAO.java
@@ -1,16 +1,42 @@
 package com.sda.jdbc.todo;
 
 
+import com.sda.jdbc.commons.connection.CustomConnection;
+import com.sda.jdbc.commons.connection.MySqlConnector;
 import com.sda.jdbc.commons.entity.Employee;
+import org.apache.log4j.Logger;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeesDAO {
+    private final CustomConnection mySqlConnection;
+    private static final Logger logger = Logger.getLogger(EmployeesDAO.class);
 
     public EmployeesDAO() {
+        this.mySqlConnection = new MySqlConnector();
     }
 
     public Employee findById(int employeeId) {
+        String query = "SELECT * FROM employees WHERE employee_id = ?";
+        try (Connection connection = mySqlConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, employeeId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            logger.info(query);
+
+            if (resultSet.next()) {
+                return toEntity(resultSet);
+            }
+            resultSet.close();
+
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        }
         return null;
     }
 
@@ -31,6 +57,23 @@ public class EmployeesDAO {
     }
 
     public void deleteBatch(List<Employee> employees) {
+    }
+
+    public static Employee toEntity(ResultSet resultSet) throws SQLException {
+        Employee employee = new Employee();
+        employee.setEmployeeId(resultSet.getInt("EMPLOYEE_ID"));
+        employee.setFirstName(resultSet.getString("FIRST_NAME"));
+        employee.setLastName(resultSet.getString("LAST_NAME"));
+        employee.setEmail(resultSet.getString("EMAIL"));
+        employee.setPhoneNumber(resultSet.getString("PHONE_NUMBER"));
+        employee.setHireDate(resultSet.getDate("HIRE_DATE").toLocalDate());
+        employee.setJobId(resultSet.getString("JOB_ID"));
+        employee.setSalary(resultSet.getDouble("SALARY"));
+        employee.setCommissionPct(resultSet.getDouble("COMMISSION_PCT"));
+        employee.setManagerId(resultSet.getInt("MANAGER_ID"));
+        employee.setDepartmentId(resultSet.getInt("DEPARTMENT_ID"));
+
+        return employee;
     }
 
 }
