1ce30f964f11ff5d1b83ad1cd2e380427cdd5271
Karol011 <karollacek1@gmail.com>
2021-02-27 14:08:22 +0100

    implemented findMovieById method

--- src/main/java/com/sda/hibernate/commons/dao/MovieDao.java
+++ src/main/java/com/sda/hibernate/commons/dao/MovieDao.java
@@ -1,10 +1,14 @@
 package com.sda.hibernate.commons.dao;
 
 
+import com.sda.hibernate.commons.connection.HibernateUtil;
 import com.sda.hibernate.commons.entity.Movie;
 import com.sda.hibernate.commons.entity.Person;
 import com.sda.hibernate.commons.entity.PersonMovie;
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -13,6 +17,25 @@ public class MovieDao {
     private final Logger logger = Logger.getLogger(MovieDao.class);
 
     public Movie findById(Integer id) {
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            final String query = "FROM Movie WHERE id = :id";
+
+            Movie movie = (Movie) session.createQuery(query)
+                    .setParameter("id", id)
+                    .getSingleResult();
+
+            transaction.commit();
+            logger.info("Object found " + movie.toString());
+            return movie;
+        } catch (HibernateException e) {
+            logger.error(e.getMessage(), e);
+            if (transaction != null)
+                transaction.rollback();
+
+        }
         return null;
     }
 
--- src/main/java/com/sda/hibernate/commons/entity/Movie.java
+++ src/main/java/com/sda/hibernate/commons/entity/Movie.java
@@ -8,16 +8,31 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @NoArgsConstructor
 @Data
+@Table(name = "movies")
 public class Movie {
 
+    @Id
+    @Column(name = "movie_id")
     private Integer id;
+
+    @Column(name = "title", nullable = false)
     private String title;
+    @Column(name = "production_year")
     private Integer productionYear;
+    @Column(name = "duration")
     private Integer duration;
 
+    @ManyToOne
+    @JoinColumn(name ="country_id")
     private Country country;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
     private Genre genre;
+
+    @Transient
     private List<Person> persons = new ArrayList<>();
 }
